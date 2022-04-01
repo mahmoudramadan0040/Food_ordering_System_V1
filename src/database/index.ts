4190405b5a7e765acaca7b21058b6c36b04627f8
mahmoudramamdan0040 <mahmoudramamdan0040@gmail.com>
2022-04-01 23:26:15 +0200

    connect to db

---
+++ src/database/index.ts
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+import config from '../config/config';
+
+const sequelize = new Sequelize(config.db as string, config.db_user as string, config.db_password, {
+    host: 'localhost',
+    dialect: 'postgres' 
+});
+
+export default sequelize;
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import rateLimit from 'express-rate-limit';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import config from './config/config';
-
+import db from './database/index';
 const app =express()
 const PORT = config.port || 3000;
 const limiter = rateLimit({
@@ -19,7 +19,12 @@ app.use(limiter);
 app.use(express.json());
 app.use(morgan('common'));
 app.use(helmet());
-
+try {
+  db.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 app.use('/api',routes);
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
